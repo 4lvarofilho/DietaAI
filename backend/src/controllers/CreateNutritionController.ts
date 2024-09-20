aeda569921ae2ead4969c88c19992d4e250e3485
Alvaro Filho <alvarofilho.dev@gmail.com>
2024-09-20 03:09:54 -0300

    fix: fix connectivity and api routing

--- backend/src/controllers/CreateNutritionController.ts
+++ backend/src/controllers/CreateNutritionController.ts
@@ -1,25 +1,35 @@
-import {FastifyReply, FastifyRequest} from "fastify";
-import {CreateNutritionService} from '../services/CreateNutritionService'
-import { DataProps } from '../interfaces/DataProps'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { CreateNutritionService } from '../services/CreateNutritionService'
+
+export interface DataProps{
+  name: string;
+  weight: string;
+  height: string;
+  age: string;
+  gender: string;
+  objective: string;
+  level: string;
+}
 
 class CreateNutritionController{
-    async handle(request:FastifyRequest, reply:FastifyReply){
-        const { name, weight, height, age, gender, objective, level, comorbidity } = request.body as DataProps;
+  async handle(request: FastifyRequest, reply: FastifyReply){
+    const { name, weight, height, age, gender, objective, level } = request.body as DataProps;
 
-        const createNutrition = new CreateNutritionService();
-        const nutrition = await createNutrition.execute({
-            name,
-            weight,
-            height,
-            age,
-            gender,
-            objective,
-            level,
-            comorbidity
-        });
+    const createNutrition = new CreateNutritionService();
 
-        reply.send(nutrition);
-    }
+    const nutrition = await createNutrition.execute({
+      name,
+      weight,
+      height,
+      age,
+      objective,
+      gender,
+      level,
+    });
+
+    reply.send(nutrition);
+
+  }
 }
 
 export { CreateNutritionController }
